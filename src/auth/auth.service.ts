8c7ee434a4eb75d77a137185acc8b8c9cabf805d
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-10-23 12:04:38 -0600

    aplicar formato al código

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,12 +3,15 @@ import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
-    async hashPassword(password: string): Promise<string> {
-        const saltRounds = 10;
-        return await bcrypt.hash(password, saltRounds);
-    }
+  async hashPassword(password: string): Promise<string> {
+    const saltRounds = 10;
+    return await bcrypt.hash(password, saltRounds);
+  }
 
-    async comparePasswords(password: string, hashedPassword: string): Promise<boolean> {
-        return await bcrypt.compare(password, hashedPassword);
-    }
+  async comparePasswords(
+    password: string,
+    hashedPassword: string,
+  ): Promise<boolean> {
+    return await bcrypt.compare(password, hashedPassword);
+  }
 }
