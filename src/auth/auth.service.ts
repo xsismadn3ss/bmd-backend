275aa50f27f34ff4e9eb368580478dd96003879b
arthur <arthureli81@gmail.com>
2025-10-23 17:19:24 -0600

    feat: crear una usuario

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,17 +1,47 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import * as bcrypt from 'bcrypt';
+import { UserService } from 'src/user/user.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  async hashPassword(password: string): Promise<string> {
-    const saltRounds = 10;
-    return await bcrypt.hash(password, saltRounds);
-  }
+  constructor(
+    private usersService: UserService,
+    private jwtService: JwtService
+  ) {}
 
-  async comparePasswords(
-    password: string,
-    hashedPassword: string,
-  ): Promise<boolean> {
-    return await bcrypt.compare(password, hashedPassword);
+  // async hashPassword(password: string): Promise<string> {
+  //   const saltRounds = 10;
+  //   return bcrypt.hash(password, saltRounds);
+  // }
+
+  // async comparePasswords(
+  //   password: string,
+  //   hashedPassword: string,
+  // ): Promise<boolean> {
+  //   return bcrypt.compare(password, hashedPassword);
+  // }
+
+  async register(name: string, email: string, password: string) {
+    const existingUser = await this.usersService.findByEmail(email);
+
+    if (existingUser) {
+      throw new ConflictException('email en uso');
+    } 
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await this.usersService.create(name, email, hashedPassword);
+
+    const payload = {
+      sub: user.id,
+      email: user.email
+    }
+
+    const token = this.jwtService.sign(payload);
+
+    return {
+      acces_token: token
+    }
   }
 }
