3d522e33987a02cfd62f142c22e1ceac10f7c8b6
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-10-23 12:05:10 -0600

    importar el modulo de usuarios y el de jwt para poder usar sus servicios en el modulo de autenticacion

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { UserService } from 'src/user/user.service';
+import { UserModule } from 'src/user/user.module';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   controllers: [AuthController],
   providers: [AuthService],
   exports: [AuthService],
-  imports: [UserService],
+  imports: [UserModule, JwtModule],
 })
 export class AuthModule {}
