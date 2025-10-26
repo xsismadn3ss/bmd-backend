a0fdf9273b790d74a77038066be1f2f74c6812c7
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-10-25 23:11:39 -0600

    mejorar implementacion para servicio de usuarios

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,27 +1,55 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  NotFoundException,
+  Injectable,
+} from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateUserDTO, UserDTO } from './user.dto';
 
 @Injectable()
 export class UserService {
   constructor(private readonly prisma: PrismaService) {}
 
-  // TODO: implementar métodos para manejar usuarios
-
-  async findByEmail(email: string) {
-    return this.prisma.user.findUnique({
+  /**
+   * Filtrar usuario por email
+   * @param email email de usuario
+   * @returns Usuario filtrado por email
+   * @throws error `404` en caso no se encuentre el usuario
+   */
+  async findByEmail(email: string): Promise<UserDTO> {
+    const user = await this.prisma.user.findUnique({
       where: {
-        email
-      }
+        email,
+      },
     });
+
+    if (!user) {
+      throw new NotFoundException('Usuario no encontrado');
+    }
+    const { password, ...userWithoutPassword } = user;
+    return userWithoutPassword as UserDTO;
   }
 
-  async create(name: string, email: string, password: string) {
-    return this.prisma.user.create({
-      data: {
-        name,
-        email,
-        password
+  /**
+   * Registrar usuario
+   * @param data DTO con datos para registrar un nuevo usuario
+   * @returns usuario creado
+   * @throws error `405` en caso se intente crear un usuario con un email ya ocupado
+   */
+  async create(data: CreateUserDTO): Promise<UserDTO> {
+    try {
+      const createdUser = await this.prisma.user.create({
+        data: {
+          ...data,
+        },
+      });
+      const { password, ...userWithoutPassword } = createdUser;
+      return userWithoutPassword as UserDTO;
+    } catch (error) {
+      if (error.code === 'P2002') {
+        throw new ConflictException('Ya existe una cuenta con este email');
       }
-    });
+      throw error;
+    }
   }
 }
