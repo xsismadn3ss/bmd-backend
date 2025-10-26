29d84b532d4c387eb455e64b487e255968ea48b8
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-10-25 23:12:13 -0600

    eliminar atributo

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -11,7 +11,6 @@ import { JwtModule } from '@nestjs/jwt';
   imports: [
     UserModule,
     JwtModule.register({
-      global: true,
       secret: process.env.JWT_SECRET,
       signOptions: { expiresIn: '60s' },
     })],
