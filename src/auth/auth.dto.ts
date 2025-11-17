b536527f7f0de1f9e3d377828528f7c62f085cc0
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-11-16 19:57:38 -0600

    📝  agregar anotaciones de swagger

--- src/auth/auth.dto.ts
+++ src/auth/auth.dto.ts
@@ -1,21 +1,37 @@
 import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class AuthLoginDto {
+  @ApiProperty({
+    example: 'satoshi@example.com',
+    description: 'Correo electrónico del usuario',
+  })
   @IsNotEmpty()
   @IsString()
   @IsEmail()
   email: string;
 
+  @ApiProperty({
+    example: 'strongPassword123',
+    description: 'Contraseña del usuario',
+  })
   @IsNotEmpty()
   @IsString()
   password: string;
 }
 
 export class AuthCredentialsDTO {
+  @ApiProperty({
+    example: 'Satoshi Nakamoto',
+    description: 'Nombre del usuario',
+  })
   @IsNotEmpty()
   @IsString()
   name: string;
 
+  @ApiProperty({
+    description: 'JSON Web Token',
+  })
   @IsNotEmpty()
   @IsString()
   token: string;
