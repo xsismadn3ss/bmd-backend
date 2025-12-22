f6369537bd3a179b8058bbf17cd3b8f9edb2d1d4
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-12-22 07:59:23 -0600

    ♻️ Cambiar DTO de interfaz a una clase para agregar documentación de swagger

--- src/user/profile.dto.ts
+++ src/user/profile.dto.ts
@@ -1,7 +1,23 @@
-export interface ProfileDTO {
-    name: string;
-    email: string;
-    roles: string[],
-    createdAt: Date,
-    updatedAt: Date
+import { ApiProperty } from '@nestjs/swagger';
+
+export class ProfileDTO {
+  @ApiProperty({ example: 'John Doe', description: 'Nombre del usuario' })
+  name: string;
+  @ApiProperty({
+    example: 'john.doe@example.com',
+    description: 'Correo electrónico del usuario',
+  })
+  email: string;
+  @ApiProperty({ example: ['USER'], description: 'Roles del usuario' })
+  roles: string[];
+  @ApiProperty({
+    example: '2025-12-22T07:58:05.000Z',
+    description: 'Fecha de creación del usuario',
+  })
+  createdAt: Date;
+  @ApiProperty({
+    example: '2025-12-22T07:58:05.000Z',
+    description: 'Fecha de actualización del usuario',
+  })
+  updatedAt: Date;
 }
